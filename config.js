0340b4dcb825fe2db364990f45e52a83496a6165
bobjader193-source <bobjader193@gmail.com>
2026-09-05 16:54:57 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.CCI8_SUPABASE_CONFIG = {
-  url: 'https://tmyjxdmipwlusciagtuc.supabase.co',
+  url: 'https://tmyjxdmipwlusciaqtuc.supabase.co',
   key: 'sb_publishable_J_B5RCJlUnPoelOXn7iMuw_rStJ8JwI'
 };
